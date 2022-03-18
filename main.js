2a601acc0a24f1b9e8fd0afb60b8fb45c25407b3
Writaja <74670011+Writaja@users.noreply.github.com>
2022-03-18 17:42:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
 img="";
+status="";
+object=[];
 function preload(){
 img=loadImage("dog_cat.jpg");
 }
 function setup(){
 canvas=createCanvas(640,420);
 canvas.center();
+objectdetector=ml5.objectDetector("cocossd",modelloaded);
+document.getElementById("status").innerHTML="status:detecting objects";
 
 }
 function draw(){
 image(img,0,0,640,420);
-fill("#ff0000");
-text("dog",90,70);
-noFill();
-stroke("#ff0000")
-rect(70,50,450,350);
 
+if(status !=""){
+    console.log("object=",object);
+for(i=0; i<object.length; i++){
+    document.getElementById("status").innerHTML="status: objects dected";
+    fill("#ff0000");
+    percent=floor(object[i].confidence*100);
+    text(object[i].label+" "+percent+"%",object[i].x+15,object[i].y+15);
+    noFill();
+    stroke("#ff0000");
+    rect(object[i].x,object[i].y,object[i].width,object[i].height);
+}
+}
+
+}
+function modelloaded(){
+    console.log("modelloaded");
+status=true;
+objectdetector.detect(img,gotresult);
+}
+function gotresult(error,results){
+    if(error){
+        console.log(error);
+
+    }
+    else{
+        console.log(results);
+       object=results;
+
+    }
+    
 }
